592912db14bc96516846a49093d78b17d2b2468d
Vítor Sousa <vitortls93@gmail.com>
2026-07-26 12:24:29 -0300

    v1.6.1 - SHA-256 MITM protection + build automation + config title update

--- build.js
+++ build.js
@@ -1,6 +1,35 @@
-// ==UserScript==
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const { execSync } = require('child_process');
+
+const SRC = path.join(__dirname, 'src', 'Dark Sentinel.user.js');
+const CONFIG = path.join(__dirname, 'obfuscator.config.json');
+const OUT_OBFUSCATED = path.join(__dirname, 'dist', 'sentinel.js');
+const OUT_LOADER = path.join(__dirname, 'dist', 'DarkSentinel.obs.user.js');
+
+const GITHUB_RAW = 'https://raw.githubusercontent.com/darkytcho/darksentinel/main/dist/sentinel.js';
+
+console.log('[build] Lendo source...');
+const source = fs.readFileSync(SRC, 'utf8');
+
+console.log('[build] Obfuscando...');
+const obfuscatorBin = path.join(__dirname, 'node_modules', '.bin', 'javascript-obfuscator');
+const obfuscated = execSync(
+    `"${obfuscatorBin}" "${SRC}" --config "${CONFIG}"`,
+    { encoding: 'utf8', maxBuffer: 10 * 1024 * 1024 }
+);
+
+console.log('[build] Calculando SHA-256...');
+const hash = crypto.createHash('sha256').update(obfuscated).digest('hex');
+console.log('[build] Hash:', hash);
+
+console.log('[build] Gerando loader...');
+const version = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).version;
+
+const loader = `// ==UserScript==
 // @name         Dark Sentinel
-// @version      1.6.1
+// @version      ${version}
 // @author       Dark Rebel
 // @description  Sentinelas automatizadas para Grepolis
 // @updateURL    https://github.com/darkytcho/darksentinel/releases/latest/download/DarkSentinel.obs.user.js
@@ -12,8 +41,8 @@
 
 (function () {
 	'use strict';
-	var EXPECTED_HASH = '126790fae4aac3fbe759bea269176cae89dc5b5fbf7ffcbfec6e425fd6abfa6f';
-	var u = 'https://raw.githubusercontent.com/darkytcho/darksentinel/main/dist/sentinel.js?' + Date.now();
+	var EXPECTED_HASH = '${hash}';
+	var u = '${GITHUB_RAW}?' + Date.now();
 
 	if (location.protocol !== 'https:') {
 		console.error('[Dark Sentinel] Abortado: conexao nao e HTTPS.');
@@ -51,3 +80,8 @@
 			console.error('[Dark Sentinel] Falha ao carregar:', e.message);
 		});
 })();
+`;
+
+fs.writeFileSync(OUT_LOADER, loader, 'utf8');
+console.log('[build] Loader gerado:', OUT_LOADER);
+console.log('[build] Build concluido com sucesso!');
